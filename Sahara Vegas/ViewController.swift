2d2c29c4da5580d1f6245bebc733e95a7246631d
Leotfs123 <lechen@tfs.ca>
2023-03-29 14:38:49 -0400

    Initial Commit

---
+++ Sahara Vegas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sahara Vegas
+//
+//  Created by Leo Chen on 2023-03-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
