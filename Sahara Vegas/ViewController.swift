437e3cb32af87b0d9eaa32163c5adf54421a42c8
Leotfs123 <lechen@tfs.ca>
2023-04-03 09:41:02 -0400

    Lundi Avril 3

--- Sahara Vegas/ViewController.swift
+++ Sahara Vegas/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1:UIImageView!
+    @IBOutlet weak var diceImageView2:UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named:"DThree")
+        diceImageView2.image = UIImage(named:"DTwo")
+        
     }
-
-
+    
+    @IBAction func rollButtonPressed(_  sender: UIButton){
+        let diceArray =
+        [UIImage(named:"DOne.png"),UIImage(named:"DTwo.png"),UIImage(named:"DThree.png"),UIImage(named:"DFour.png"),UIImage(named:"DFive.png"),UIImage(named:"DSix.png")]
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
 }
 
